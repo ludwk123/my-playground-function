e7c3c7b51b01dd73e9951f573abfd393c0a3014e
wessey <wessey@comcast.net>
2015-10-01 07:31:29 -0400

    add file for my playground function

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func add(numA : Double, numB : Double) -> Double{
+    var sum = numA + numB
+    return sum
+}
+
+add(20.05, 15.90)
+
+func subtract(subA : Int, subB : Int)   ->Int{
+    var sub = subA - subB
+    return sub
+}
+
+subtract(30, 13)
+
+func multiply(multiplyA:Float,multiplyB:Float) ->Float{
+    var multiply = multiplyA * multiplyB
+    return multiply
+}
+
+multiply(12, 12)
+
+
+func division(divA:Double, divB:Double) ->Double{
+    var divide = divA / divB
+    
+    retrun div
+}
+
+division(256, 2)
+
